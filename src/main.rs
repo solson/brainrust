e138127448df5cf361bd24508a16f6784d92687f
Scott Olson <scott@scott-olson.org>
2014-12-06 18:57:51 -0500

    Add support for different types of tapes.

--- src/main.rs
+++ src/main.rs
@@ -52,24 +52,72 @@ fn parse(program: &str) -> Result<Vec<Op>, ParseError> {
     }
 }
 
-fn execute<R: Reader, W: Writer>(program: Vec<Op>, input: &mut R, output: &mut W) -> IoResult<()> {
-    let mut tape = [0u8, ..1024];
-    let mut dp = 0u; // Data pointer.
+trait Tape {
+    fn go_left(&mut self);
+    fn go_right(&mut self);
+    fn inc(&mut self);
+    fn dec(&mut self);
+    fn read(&self) -> u8;
+    fn write(&mut self, byte: u8);
+}
+
+struct SimpleTape {
+    pos: uint,
+    data: Vec<u8>,
+}
+
+impl SimpleTape {
+    fn new(size: uint) -> SimpleTape {
+        SimpleTape { pos: 0, data: Vec::from_elem(size, 0u8) }
+    }
+}
+
+impl Tape for SimpleTape {
+    fn go_left(&mut self)  { self.pos -= 1; }
+    fn go_right(&mut self) { self.pos += 1; }
+    fn inc(&mut self) { self.data[self.pos] += 1; }
+    fn dec(&mut self) { self.data[self.pos] -= 1; }
+    fn read(&self) -> u8 { self.data[self.pos] }
+    fn write(&mut self, byte: u8) { self.data[self.pos] = byte; }
+}
+
+struct CircularTape {
+    pos: uint,
+    data: Vec<u8>,
+}
+
+impl CircularTape {
+    fn new(size: uint) -> CircularTape {
+        CircularTape { pos: 0, data: Vec::from_elem(size, 0u8) }
+    }
+}
+
+impl Tape for CircularTape {
+    fn go_left(&mut self)  { self.pos = (self.pos - 1) % self.data.len(); }
+    fn go_right(&mut self) { self.pos = (self.pos + 1) % self.data.len(); }
+    fn inc(&mut self) { self.data[self.pos] += 1; }
+    fn dec(&mut self) { self.data[self.pos] -= 1; }
+    fn read(&self) -> u8 { self.data[self.pos] }
+    fn write(&mut self, byte: u8) { self.data[self.pos] = byte; }
+}
+
+fn execute<R: Reader, W: Writer, T: Tape>(program: Vec<Op>, input: &mut R, output: &mut W,
+                                          tape: &mut T) -> IoResult<()> {
     let mut ip = 0u; // Instruction pointer.
 
     while ip < program.len() {
         match program[ip] {
-            Op::Inc   => tape[dp] += 1,
-            Op::Dec   => tape[dp] -= 1,
-            Op::Left  => dp -= 1,
-            Op::Right => dp += 1,
+            Op::Inc   => tape.inc(),
+            Op::Dec   => tape.dec(),
+            Op::Left  => tape.go_left(),
+            Op::Right => tape.go_right(),
             Op::Read  => match input.read_byte() {
-                Ok(byte) => tape[dp] = byte,
+                Ok(byte) => tape.write(byte),
                 Err(_)   => {} // Do nothing on EOF.
             },
-            Op::Write => try!(output.write_u8(tape[dp])),
-            Op::LoopStart(loop_end) => if tape[dp] == 0 { ip = loop_end; },
-            Op::LoopEnd(loop_start) => if tape[dp] != 0 { ip = loop_start; },
+            Op::Write => try!(output.write_u8(tape.read())),
+            Op::LoopStart(loop_end) => if tape.read() == 0 { ip = loop_end; },
+            Op::LoopEnd(loop_start) => if tape.read() != 0 { ip = loop_start; },
         }
 
         ip += 1;
@@ -91,7 +139,9 @@ fn main() {
         return;
     }
 
+    let mut tape = SimpleTape::new(1024);
+
     read_file(args[1].as_slice()).and_then(|program| {
-        execute(parse(program.as_slice()).unwrap(), &mut stdin(), &mut stdout())
+        execute(parse(program.as_slice()).unwrap(), &mut stdin(), &mut stdout(), &mut tape)
     }).unwrap();
 }
